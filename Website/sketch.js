e3f06b6496794ee4b1178bff5ac31259ca2ffad4
asagilmore <asagilmore@seattleacademy.org>
2022-09-07 11:38:23 -0700

    moved index.html to main folder added img

--- Website/sketch.js
+++ Website/sketch.js
@@ -6,4 +6,13 @@ function setup() {
 
 function draw() {
   background(220);
+  let img = createImage(400,400);
+  img.loadPixles();
+  for (let i = 0; i < img.width; i++){
+    for (let j = 0; i < img.height; j++){
+      img.set(i,j,color(0,90,102));
+    }
+  }
+  img.updatePixles();
+  Image(img,0,0)
 }
